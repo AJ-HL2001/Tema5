ca7d0853062a18d368193406d2f49f053a9518d3
Antonio Jose <57352590+AJ-HL2001@users.noreply.github.com>
2020-01-30 09:12:20 +0100

    Update Ejercicio8.java

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -12,7 +12,11 @@ import java.awt.event.WindowListener;
 
 public class Ejercicio8 extends Frame implements WindowListener, ActionListener {
 	private static final long serialVersionUID = 1L;
-	
+
+	float operador1, operador2, resultado;
+	int operacion;
+	boolean segundo, fin;
+
 	Button btn1 = new Button("1");
 	Button btn2 = new Button("2");
 	Button btn3 = new Button("3");
@@ -29,15 +33,13 @@ public class Ejercicio8 extends Frame implements WindowListener, ActionListener 
 	Button btnPor = new Button("*");
 	Button btnPunto = new Button(".");
 	Button btnIgual = new Button("=");
-	
+
 	TextField txtResultado = new TextField();
-	double resultado = 0;
-	
-	
+
 	public Ejercicio8() {
 		setLayout(new FlowLayout());
 		setTitle("Calculadora");
-		
+
 		setLayout(new GridLayout(5,4));
 		add(btn1);
 		add(btn2);
@@ -56,7 +58,7 @@ public class Ejercicio8 extends Frame implements WindowListener, ActionListener 
 		add(btnIgual);
 		add(btnEntre);
 		add(txtResultado);
-		
+
 		btn1.addActionListener(this);
 		btn2.addActionListener(this);
 		btn3.addActionListener(this);
@@ -73,20 +75,152 @@ public class Ejercicio8 extends Frame implements WindowListener, ActionListener 
 		btnPor.addActionListener(this);
 		btnPunto.addActionListener(this);
 		btnIgual.addActionListener(this);
-		
+
 		addWindowListener(this);
 		setSize(240,300);
 		setLocationRelativeTo(null);
 		setVisible(true);
+		setResizable(false);
 	}
 	public static void main(String[] args) {
 		new Ejercicio8();
 	}
-	
+
 	public void actionPerformed(ActionEvent ae) {
 		Object a = ae.getSource();
+		if(a.equals(btn1)) {
+			if(segundo) {
+				operador2 = 1;
+			}
+			else {
+				operador1 = 1;
+			}
+		}
+		if(a.equals(btn2)) {
+			if(segundo) {
+				operador2 = 2;
+			}
+			else {
+				operador1 = 2;
+			}
+		}
+		if(a.equals(btn3)) {
+			if(segundo) {
+				operador2 = 3;
+			}
+			else {
+				operador1 = 3;
+			}
+		}
+		if(a.equals(btnMas)) {
+			//Suma
+			segundo = true;
+			operacion = 1;
+		}
+		if(a.equals(btn4)) {
+			if(segundo) {
+				operador2 = 4;
+			}
+			else {
+				operador1 = 4;
+			}
+		}
+		if(a.equals(btn5)) {
+			if(segundo) {
+				operador2 = 5;
+			}
+			else {
+				operador1 = 5;
+			}
+		}
+		if(a.equals(btn6)) {
+			if(segundo) {
+				operador2 = 6;
+			}
+			else {
+				operador1 = 6;
+			}
+		}
+		if(a.equals(btnMenos)) {
+			//Resta
+			segundo = true;
+			operacion = 2;
+		}
+		if(a.equals(btn7)) {
+			if(segundo) {
+				operador2 = 7;
+			}
+			else {
+				operador1 = 7;
+			}
+		}
+		if(a.equals(btn8)) {
+			if(segundo) {
+				operador2 = 8;
+			}
+			else {
+				operador1 = 8;
+			}
+		}
+		if(a.equals(btn9)) {
+			if(segundo) {
+				operador2 = 9;
+			}
+			else {
+				operador1 = 9;
+			}
+		}
+		if(a.equals(btnPor)) {
+			//Multiplicacion
+			segundo = true;
+			operacion = 3;
+		}
+		if(a.equals(btnPunto)) {
+			//Decimales
+		}
+		if(a.equals(btn0)) {
+			if(segundo) {
+				operador2 = 0;
+			}
+			else {
+				operador1 = 0;
+			}
+		}
+		if(a.equals(btnEntre)) {
+			//Division
+			segundo = true;
+			operacion = 4;
+		}
+		if(a.equals(btnIgual)) {
+			switch(operacion) {
+			case 1:
+				resultado = operador1 + operador2;
+				fin = true;
+				break;
+			case 2:
+				resultado = operador1 - operador2;
+				fin = true;
+				break;
+			case 3:
+				resultado = operador1 * operador2;
+				fin = true;
+				break;
+			case 4:
+				resultado = operador1 / operador2;
+				fin = true;
+				break;
+			}
+			segundo = false;
+			operacion = 0;
+			fin = true;
+		}
+		if(fin) {
+			txtResultado.setText(String.valueOf(resultado));
+			fin = false;
+		}
 	}
-	
+
+
 	public void windowActivated(WindowEvent arg0) {}
 	public void windowClosed(WindowEvent arg0) {}
 	public void windowClosing(WindowEvent arg0) {
