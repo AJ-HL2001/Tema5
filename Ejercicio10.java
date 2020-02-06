9c10abe880c6cd715bbe201c109d103d417d8de0
Antonio Jose <57352590+AJ-HL2001@users.noreply.github.com>
2020-02-06 14:42:39 +0100

    Update Ejercicio10.java

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -1,18 +1,25 @@
 package es.studium.Ejercicios;
 
+import java.awt.BorderLayout;
 import java.awt.Button;
-import java.awt.Dialog;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.Label;
+import java.awt.Panel;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-public class Ejercicio10 extends Frame implements WindowListener, ActionListener {
+public class Ejercicio10 extends Frame implements WindowListener, ActionListener, MouseListener, KeyListener {
 	private static final long serialVersionUID = 1L;
 
 	Button btnInicio = new Button("Iniciar");
@@ -21,8 +28,6 @@ public class Ejercicio10 extends Frame implements WindowListener, ActionListener
 	TextField txtJug1 = new TextField(20);
 	TextField txtJug2 = new TextField(20);
 
-	Dialog dlgGanador = new Dialog(this, "Ganador", true);
-	Label lblGanador = new Label("");
 
 	Frame fJuego = new Frame("Tres en Raya");
 	Button btn1 = new Button("");
@@ -34,38 +39,62 @@ public class Ejercicio10 extends Frame implements WindowListener, ActionListener
 	Button btn7 = new Button("");
 	Button btn8 = new Button("");
 	Button btn9 = new Button("");
+	Panel juego = new Panel();
 
+	TextField txtGanador = new TextField(25); 
+	Button btnNuevo = new Button("Reiniciar");
+	Panel informacion = new Panel();
 
 	int turno = 0;
+	boolean fin = false;
 
 	public Ejercicio10() {
 		setLayout(new FlowLayout());
 		setTitle("Inicio");
 		add(lblJug1);
 		add(txtJug1);
+		txtJug1.addMouseListener(this);
 		add(lblJug2);
 		add(txtJug2);
+		txtJug2.addMouseListener(this);
 		add(btnInicio);
+		btnInicio.setPreferredSize(new Dimension(150,20));
+		btnInicio.setForeground(Color.BLUE);
+		btnInicio.setBackground(Color.WHITE);
+		lblJug1.setForeground(Color.BLUE);
+		lblJug2.setForeground(Color.RED);
+		txtJug1.setBackground(Color.WHITE);
+		txtJug2.setBackground(Color.WHITE);
+		setBackground(Color.LIGHT_GRAY);
+		setSize(300,130);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setResizable(false); 
 
-		fJuego.setLayout(new GridLayout(3,3));
-		fJuego.add(btn1);
-		fJuego.add(btn2);
-		fJuego.add(btn3);
-		fJuego.add(btn4);
-		fJuego.add(btn5);
-		fJuego.add(btn6);
-		fJuego.add(btn7);
-		fJuego.add(btn8);
-		fJuego.add(btn9);
+		juego.setLayout(new GridLayout(3,3));
+		juego.add(btn1);
+		juego.add(btn2);
+		juego.add(btn3);
+		juego.add(btn4);
+		juego.add(btn5);
+		juego.add(btn6);
+		juego.add(btn7);
+		juego.add(btn8);
+		juego.add(btn9);
+
+		informacion.setLayout(new FlowLayout());
+		txtGanador.setEnabled(false);
+		informacion.add(txtGanador);
+		informacion.add(btnNuevo);
+		btnNuevo.setBackground(Color.GREEN);
+		btnNuevo.setForeground(Color.BLUE);
+
+		fJuego.add(informacion, BorderLayout.NORTH);
+		fJuego.add(juego, BorderLayout.CENTER);
+		fJuego.setBackground(Color.darkGray);
 		fJuego.setSize(300,300);
 		fJuego.setLocationRelativeTo(null);
 		fJuego.setResizable(false);
-
-		dlgGanador.setLayout(new FlowLayout());
-		dlgGanador.add(lblGanador);
-		dlgGanador.setSize(150,100);
-		dlgGanador.setLocationRelativeTo(null);
-		dlgGanador.setResizable(false);
 
 		btn1.addActionListener(this);
 		btn2.addActionListener(this);
@@ -77,14 +106,13 @@ public class Ejercicio10 extends Frame implements WindowListener, ActionListener
 		btn8.addActionListener(this);
 		btn9.addActionListener(this);
 		btnInicio.addActionListener(this);
+		btnNuevo.addActionListener(this);
 
-		dlgGanador.addWindowListener(this);
 		fJuego.addWindowListener(this);
 		addWindowListener(this);
-		setSize(300,140);
-		setLocationRelativeTo(null);
-		setVisible(true);
-		setResizable(false);
+		addKeyListener(this);
+		txtJug1.addKeyListener(this);
+		txtJug2.addKeyListener(this);
 	}
 
 	public static void main(String[] args) {
@@ -94,13 +122,25 @@ public class Ejercicio10 extends Frame implements WindowListener, ActionListener
 	public void actionPerformed(ActionEvent ae) {
 		Object a = ae.getSource();
 		if(a.equals(btnInicio)) {
-			fJuego.setVisible(true);
+			if((txtJug1.getText().length() != 0)&&(txtJug2.getText().length() != 0)) {
+				fJuego.setVisible(true);
+			}
+			else if(txtJug1.getText().length() == 0) {
+				txtJug1.setText("Indica el Jug1");
+			}
+			else {
+				txtJug2.setText("Indica el Jug2");
+			}
+		}
+		else if(a.equals(btnNuevo)) {
+			Nuevo();
 		}
 		else {
 			Button boton = (Button) ae.getSource();
 			if(turno == 0) {
 				if(boton.getLabel().equals("")) {
 					boton.setLabel("X");
+					boton.setBackground(Color.BLUE);
 					boton.setEnabled(false);
 					turno = 1;
 				}
@@ -109,6 +149,7 @@ public class Ejercicio10 extends Frame implements WindowListener, ActionListener
 				if(turno == 1) {
 					boton.setLabel("O");
 					boton.setEnabled(false);
+					boton.setBackground(Color.RED);
 					turno = 0;
 				}
 			}
@@ -123,11 +164,6 @@ public class Ejercicio10 extends Frame implements WindowListener, ActionListener
 			fJuego.setVisible(false);
 			Nuevo();
 		}
-		else if(dlgGanador.isActive()) {
-			dlgGanador.setVisible(false);
-			fJuego.setVisible(false);
-			Nuevo();
-		}
 		else {
 			System.exit(0);
 		}
@@ -136,6 +172,35 @@ public class Ejercicio10 extends Frame implements WindowListener, ActionListener
 	public void windowDeiconified(WindowEvent arg0) {}
 	public void windowIconified(WindowEvent arg0) {}
 	public void windowOpened(WindowEvent arg0) {}
+
+	public void mouseClicked(MouseEvent me) {
+		if(txtJug1.getText().equals("Indica el Jug1")) {
+			txtJug1.selectAll();
+			txtJug1.setText("");
+		}
+		if(txtJug2.getText().equals("Indica el Jug2")) {
+			txtJug2.selectAll();
+			txtJug2.setText("");
+		}
+	}
+	public void mouseEntered(MouseEvent arg0) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+
+	public void keyPressed(KeyEvent ke) {
+		if(ke.getKeyCode()==KeyEvent.VK_ENTER) {
+			if((txtJug1.getText().length() != 0)&&(txtJug2.getText().length() != 0)) {
+				fJuego.setVisible(true);
+			}
+			if((txtJug1.getText().length() != 0)&&(txtJug2.getText().length() == 0)) {
+				txtJug2.requestFocus();
+			}
+		}
+	}
+	public void keyReleased(KeyEvent arg0) {}
+	public void keyTyped(KeyEvent arg0) {}
+
 
 	public void Verificar() {
 		Object b1 = btn1.getLabel();
@@ -153,125 +218,139 @@ public class Ejercicio10 extends Frame implements WindowListener, ActionListener
 		if((b1.equals("X"))&&(b2.equals("X"))&&(b3.equals("X"))) {
 			ganador = 1;
 			if(ganador == 1) {
-				lblGanador.setText("Ha ganado " + txtJug1.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug1.getText());
+				fin = true;
 			}
 		}
 		if((b4.equals("X"))&&(b5.equals("X"))&&(b6.equals("X"))) {
 			ganador = 1;
 			if(ganador == 1) {
-				lblGanador.setText("Ha ganado " + txtJug1.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug1.getText());
+				fin = true;
 			}
 		}
 		if((b7.equals("X"))&&(b8.equals("X"))&&(b9.equals("X"))) {
 			ganador = 1;
 			if(ganador == 1) {
-				lblGanador.setText("Ha ganado " + txtJug1.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug1.getText());
+				fin = true;
 			}
 		}
 		if((b1.equals("X"))&&(b4.equals("X"))&&(b7.equals("X"))) {
 			ganador = 1;
 			if(ganador == 1) {
-				lblGanador.setText("Ha ganado " + txtJug1.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug1.getText());
+				fin = true;
 			}
 		}
 		if((b2.equals("X"))&&(b5.equals("X"))&&(b8.equals("X"))) {
 			ganador = 1;
 			if(ganador == 1) {
-				lblGanador.setText("Ha ganado " + txtJug1.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug1.getText());
+				fin = true;
 			}
 		}
 		if((b3.equals("X"))&&(b6.equals("X"))&&(b9.equals("X"))) {
 			ganador = 1;
 			if(ganador == 1) {
-				lblGanador.setText("Ha ganado " + txtJug1.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug1.getText());
+				fin = true;
 			}
 		}
 		if((b1.equals("X"))&&(b5.equals("X"))&&(b9.equals("X"))) {
 			ganador = 1;
 			if(ganador == 1) {
-				lblGanador.setText("Ha ganado " + txtJug1.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug1.getText());
+				fin = true;
 			}
 		}
 		if((b3.equals("X"))&&(b5.equals("X"))&&(b7.equals("X"))) {
 			ganador = 1;
 			if(ganador == 1) {
-				lblGanador.setText("Ha ganado " + txtJug1.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug1.getText());
+				fin = true;
 			}
 		}
 		//Ganador Jugador 2
 		if((b1.equals("O"))&&(b2.equals("O"))&&(b3.equals("O"))) {
 			ganador = 2;
 			if(ganador == 2) {
-				lblGanador.setText("Ha ganado " + txtJug2.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug2.getText());
+				fin = true;
 			}
 		}
 		if((b4.equals("O"))&&(b5.equals("O"))&&(b6.equals("O"))) {
 			ganador = 2;
 			if(ganador == 2) {
-				lblGanador.setText("Ha ganado " + txtJug2.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug2.getText());
+				fin = true;
 			}
 		}
 		if((b7.equals("O"))&&(b8.equals("O"))&&(b9.equals("O"))) {
 			ganador = 2;
 			if(ganador == 2) {
-				lblGanador.setText("Ha ganado " + txtJug2.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug2.getText());
+				fin = true;
 			}
 		}
 		if((b1.equals("O"))&&(b4.equals("O"))&&(b7.equals("O"))) {
 			ganador = 2;
 			if(ganador == 2) {
-				lblGanador.setText("Ha ganado " + txtJug2.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug2.getText());
+				fin = true;
 			}
 		}
 		if((b2.equals("O"))&&(b5.equals("O"))&&(b8.equals("O"))) {
 			ganador = 2;
 			if(ganador == 2) {
-				lblGanador.setText("Ha ganado " + txtJug2.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug2.getText());
+				fin = true;
 			}
 		}
 		if((b3.equals("O"))&&(b6.equals("O"))&&(b9.equals("O"))) {
 			ganador = 2;
 			if(ganador == 2) {
-				lblGanador.setText("Ha ganado " + txtJug2.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug2.getText());
+				fin = true;
 			}
 		}
 		if((b1.equals("O"))&&(b5.equals("O"))&&(b9.equals("O"))) {
 			ganador = 2;
 			if(ganador == 2) {
-				lblGanador.setText("Ha ganado " + txtJug2.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug2.getText());
+				fin = true;
 			}
 		}
 		if((b3.equals("O"))&&(b5.equals("O"))&&(b7.equals("O"))) {
 			ganador = 2;
 			if(ganador == 2) {
-				lblGanador.setText("Ha ganado " + txtJug2.getText());
-				dlgGanador.setVisible(true);
+				txtGanador.setText("Ha ganado " + txtJug2.getText());
+				fin = true;
 			}
 		}
 		//En caso de Empate
 		if(ganador == 0) {
 			if((b1!= "")&&(b2!= "")&&(b3!= "")&&(b4!= "")&&(b5!= "")&&(b6!= "")&&(b7!= "")&&(b8!= "")&&(b9!= "")) {
-				lblGanador.setText("EMPATE");
-				dlgGanador.setVisible(true);
+				txtGanador.setText("EMPATE");
+				fin = true;
 			}
+		}
+		if(fin) {
+			btn1.setEnabled(false);
+			btn2.setEnabled(false);
+			btn3.setEnabled(false);
+			btn4.setEnabled(false);
+			btn5.setEnabled(false);
+			btn6.setEnabled(false);
+			btn7.setEnabled(false);
+			btn8.setEnabled(false);
+			btn9.setEnabled(false);
 		}
 	}
 	public void Nuevo() {
+		fin = false;
+		txtGanador.selectAll();
+		txtGanador.setText("");
 		btn1.setLabel("");
 		btn2.setLabel("");
 		btn3.setLabel("");
@@ -290,5 +369,14 @@ public class Ejercicio10 extends Frame implements WindowListener, ActionListener
 		btn7.setEnabled(true);
 		btn8.setEnabled(true);
 		btn9.setEnabled(true);
+		btn1.setBackground(Color.WHITE);
+		btn2.setBackground(Color.WHITE);
+		btn3.setBackground(Color.WHITE);
+		btn4.setBackground(Color.WHITE);
+		btn5.setBackground(Color.WHITE);
+		btn6.setBackground(Color.WHITE);
+		btn7.setBackground(Color.WHITE);
+		btn8.setBackground(Color.WHITE);
+		btn9.setBackground(Color.WHITE);
 	}
 }
